b32f0a92bdf8d56ce3ca7d88e4dc9dac54bbb32a
jr-98 <jrtillaguango@gmail.com>
2022-11-07 09:49:58 -0500

    Creacion de Uusuarios

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,60 +1,85 @@
 import React, { Component } from 'react';
-import { PostData } from '../../services/PostData';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
 import { Grid, Typography } from '@material-ui/core'
-// import {Redirect} from 'react-router-dom';
 import './Signup.css';
 
 class Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: '',
+      direction: '',
+      email: '',
+      phone: '',
       username: '',
       password: '',
-      email: '',
-      name: '',
-      redirectToReferrer: false
+      redirectToReferrer: false,
+      isFormComplete: true
     };
     this.signup = this.signup.bind(this);
     this.onChange = this.onChange.bind(this);
   }
   signup() {
-    if (this.state.username && this.state.password && this.state.email && this.state.name) {
-      PostData('signup', this.state).then((result) => {
-        let responseJson = result;
-        if (responseJson.userData) {
-          sessionStorage.setItem('userData', JSON.stringify(responseJson));
-          this.setState({ redirectToReferrer: true });
+    if (this.state.name && this.state.direction && this.state.email && this.state.phone && this.state.username && this.state.password) {
+      axios.post('http://162.243.161.34:8080/api/users/create', {
+        "name": this.state.username,
+        "direction": this.state.direction,
+        "phone": this.state.phone,
+        "email": this.state.email,
+        "password": this.state.password,
+        "type": "user"
+      }
+      ).then((response) => {
+        console.log("🚀 ~ file: Signup.jsx ~ line 35 ~ Signup ~ ).then ~ response", response)
+        alert('Procesando')
+        if (response) {
+          this.setState({ redirectToReferrer: true })
+          alert(response.data.message)
         }
+        this.setState({ redirectToReferrer: true })
+      }).catch((error) => {
+        console.log('error:', error);
       });
     }
   }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
+    if (this.state.name && this.state.direction && this.state.email && this.state.phone && this.state.username && this.state.password) {
+      this.setState({ isFormComplete: false })
+    }
   }
   render() {
-    if (this.state.redirectToReferrer || sessionStorage.getItem('userData')) {
-      // return (<Redirect to={'/home'}/>)
+    if (this.state.redirectToReferrer) {
+      return <Navigate to='/home' />
     }
     return (
       <Grid container>
         <Grid item className='img-login' sx={12} sm={6} >
-          <img ref='logo-inicial' src='/assets/img/9812.png'></img>
+          <img alt='logo-inicial' src='/assets/img/9812.png'></img>
         </Grid>
         <Grid item container sx={12} sm={6} className="row" id="Body">
           <Grid item className="medium-5 columns left box-container">
-            <Grid item className='container-form'>
-              <h4>Registro</h4>
-              <label>Email</label>
-              <input type="text" name="email" placeholder="Email" onChange={this.onChange} />
-              <label>Nombre</label>
-              <input type="text" name="name" placeholder="Name" onChange={this.onChange} />
-              <label>Nombre de usuario</label>
-              <input type="text" name="username" placeholder="Username" onChange={this.onChange} />
-              <label>Contraseña</label>
-              <input type="password" name="password" placeholder="Password" onChange={this.onChange} />
-              <input type="submit" className="button" value="Registrate" onClick={this.signup} />
-              <a href="/">Cancelar</a>
-            </Grid>
+            <form onSubmit={this.signup}>
+              <Grid item className='container-form'>
+                <h4>Registro</h4>
+                <label>Nombre</label>
+                <input type="text" name="name" placeholder="Name" onChange={this.onChange} />
+                <label>Direccion</label>
+                <input type="text" name="direction" placeholder="Dirección" onChange={this.onChange} />
+                <label>Email</label>
+                <input type="text" name="email" placeholder="Email" onChange={this.onChange} />
+                <label>Teléfono</label>
+                <input type="text" name="phone" placeholder="Teléfono" onChange={this.onChange} />
+                <label>Nombre de usuario</label>
+                <input type="text" name="username" placeholder="Username" onChange={this.onChange} />
+                <label>Contraseña</label>
+                <input type="password" name="password" placeholder="Password" onChange={this.onChange} />
+                <input type="submit" disabled={this.state.isFormComplete} className="button" value="Registrate" onClick={this.signup} />
+                <a href="/">Cancelar</a>
+              </Grid>
+            </form>
           </Grid >
         </Grid>
       </Grid>
